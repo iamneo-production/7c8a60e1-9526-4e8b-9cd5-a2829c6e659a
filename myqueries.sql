defcfb1073992e76990569ab2eeac185796a44a9
Jella-Bhaskar <bhaskar180302@gmail.com>
2023-07-09 05:36:10 +0000

    saample query

--- myqueries.sql
+++ myqueries.sql
@@ -3,3 +3,10 @@ select count(*) from TELECOM;
 select product,count(product) from Telecom group by product;
 
 select CUSTOMERID from telecom;
+
+desc telecom;
+
+select circle,count(*) from Telecom group by circle;
+
+create index ids on telecom(zone);
+select product from telecom order by zone;
