fe4a4524d3ca3746696a890c055359a31e3d6182
vaishnavijakkula <jakkulavaishnavireddy@gmail.com>
2023-07-09 18:07:25 +0000

    4

--- myqueries.sql
+++ myqueries.sql
@@ -2,3 +2,7 @@ select "Customer Name" from TELECOM
 where PRODUCT='Digital Subscriber Line';
 select CUSTOMERID,"Customer Name" from TELECOM
 where "Customer Name" like'sa%';
+select CUSTOMERID,"Customer Name" from TELECOM
+where "Service Segment"='Gold';
+select "Customer Name" from TELECOM
+where "ZONE"='EASTERN';
