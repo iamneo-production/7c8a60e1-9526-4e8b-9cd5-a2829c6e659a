295ad0100ef95b18eeb14757a62adb175dca39dc
vaishnavijakkula <jakkulavaishnavireddy@gmail.com>
2023-07-10 06:57:33 +0000

    5

--- myqueries.sql
+++ myqueries.sql
@@ -4,5 +4,7 @@ select CUSTOMERID,"Customer Name" from TELECOM
 where "Customer Name" like'sa%';
 select CUSTOMERID,"Customer Name" from TELECOM
 where "Service Segment"='Gold';
+select PRODUCT, count(PRODUCT) from TELECOM
+group by PRODUCT;
 select "Customer Name" from TELECOM
 where "ZONE"='EASTERN';
