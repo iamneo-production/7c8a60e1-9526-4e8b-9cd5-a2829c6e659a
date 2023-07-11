200bbd101e7bedc674cdd0526281f43dc712b6a3
Himaja0228 <seelamhimaja298@gmail.com>
2023-07-11 08:28:05 +0000

    EDITED CAPS

--- Myqueries.sql
+++ Myqueries.sql
@@ -1,10 +1,10 @@
-select "Customer Name" from TELECOM
+select "CUSTOMER NAME" from TELECOM
 where Product='Digital Subscriber Line';
 select CustomerId, "Customer Name" from TELECOM
-where "Customer Name" like 'sa%';
-select CustomerId, "Customer Name" from TELECOM
+where "CUSTOMER NAME" like 'sa%';
+select CustomerId, "CUSTOMER NAME" from TELECOM
 where "Service Segment"='Gold';
 select Product, count(Product) from TELECOM
 group by Product;
-select "Customer Name" from TELECOM 
+select "CUSTOMER NAME" from TELECOM 
 where ZONE='Mountain';
