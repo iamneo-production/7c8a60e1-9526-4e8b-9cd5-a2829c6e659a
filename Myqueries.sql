c7de36c671b80994c8fbf0892a8d3bd9c7a6d60e
Himaja0228 <seelamhimaja298@gmail.com>
2023-07-11 13:44:29 +0000

    5 quetions

--- Myqueries.sql
+++ Myqueries.sql
@@ -1,10 +1,10 @@
-select "CUSTOMER NAME" from TELECOM
+select "Customer Name" from TELECOM
 where Product='Digital Subscriber Line';
 select CustomerId, "Customer Name" from TELECOM
-where "CUSTOMER NAME" like 'sa%';
-select CustomerId, "CUSTOMER NAME" from TELECOM
+where "Customer Name" like 'sa%';
+select CustomerId, "Customer Name" from TELECOM
 where "Service Segment"='Gold';
 select Product, count(Product) from TELECOM
 group by Product;
-select "CUSTOMER NAME" from TELECOM 
+select "customer Name" from TELECOM 
 where ZONE='Mountain';
