e102206c770897f62c0370382e45a688daa624c3
Himaja0228 <132827219+Himaja0228@users.noreply.github.com>
2023-07-11 19:48:25 +0530

    Myqueries.sql

--- Myqueries.sql
+++ Myqueries.sql
@@ -6,9 +6,6 @@ select CustomerId, "Customer Name" from TELECOM
 where "Service Segment"='Gold';
 select Product, count(Product) from TELECOM
 group by Product;
-<<<<<<< HEAD
 select "customer Name" from TELECOM 
-=======
 select "Customer Name" from TELECOM 
->>>>>>> origin/main
 where ZONE='Mountain';
