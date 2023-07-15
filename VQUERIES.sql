db66920c719bfdde15da6a8bba03977437209beb
JadalaVignan <20955a0421@iare.ac.in>
2023-07-15 10:53:43 +0000

    query 2

--- VQUERIES.sql
+++ VQUERIES.sql
@@ -2,6 +2,13 @@
 select "Customer Name"
 from TELECOM
 where product='Digital Subscriber Line';
+/*query for customerid and customer name whose name starts with sa'*/
+select customerid, "Customer Name"
+FROM TELECOM
+where "Customer Name" like 'sa%';
+
+
+
 
 
 
