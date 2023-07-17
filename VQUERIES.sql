cab6ded50c9b74518aebc0f207e422357eb27bfb
JadalaVignan <20955a0421@iare.ac.in>
2023-07-17 13:27:44 +0000

    table name change

--- VQUERIES.sql
+++ VQUERIES.sql
@@ -1,26 +1,26 @@
 /*query for customer names who are using product digital subscriber line*/
 select "Customer Name"
-from TELECOM
+from TELECOM_CUSTOMER
 where product='Digital Subscriber Line';
 /*query for customerid and customer name whose name starts with 'sa'*/
 select customerid, "Customer Name"
-FROM TELECOM
+FROM TELECOM_CUSTOMER
 where "Customer Name" like 'sa%';
 /*another query for customerid and customer name whose name starts with 'sa'*/
 select customerid,"Customer Name"
-from TELECOM
+from TELECOM_CUSTOMER
 where "Customer Name">='sa' and "Customer Name"<'sb';
 /*customerid and customer names beloging to the gold customer segment*/
 select customerid,"Customer Name"
-from TELECOM
+from TELECOM_CUSTOMER
 where "Service Segment"='Gold';
 /*count the customer list product wise*/
 select product, count(product) 
-from TELECOM
+from TELECOM_CUSTOMER
 group by product;
 /*another query for count the customer list product wise*/
 select product, count(*)
-from TELECOM
+from TELECOM_CUSTOMER
 group by product;
 /*query to list the customer names of the zone mountain*/
 select "Customer Name"
