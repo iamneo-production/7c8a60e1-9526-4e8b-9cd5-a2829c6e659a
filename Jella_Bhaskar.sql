f437c3f6e8a4f77ea8852319104671be40d25ddd
Jella-Bhaskar <bhaskar180302@gmail.com>
2023-07-28 06:25:34 +0000

    final queries

--- Jella_Bhaskar.sql
+++ Jella_Bhaskar.sql
@@ -1,3 +1,39 @@
+/* sql queries  before normalization */
+
+/* 1)Write a SQL query to find a list of customer names
+ who are using  the product Digital Subscriber Line? */
+
+ set timing on;
+
+select "Customer Name" from TELECOM_CUSTOMER
+ where PRODUCT='Digital Subscriber Line';
+
+/* 2)Write a SQL query to  list a customerid, customer name
+ whose name starts with 'sa'? */
+
+ select CUSTOMERID,"Customer Name" from TELECOM_CUSTOMER
+ where "Customer Name" like 'sa%';
+
+ /* 3)Write a SQL query to  list the Customer IDs and names 
+belonging to the gold customer segment? */
+
+select CUSTOMERID,"Customer Name" from TELECOM_CUSTOMER
+where "Service Segment"='Gold';
+
+/* 4) Write a SQL query to Count the Customer list product-wise?  */
+
+select PRODUCT,count(PRODUCT) from TELECOM_CUSTOMER
+group by PRODUCT;
+
+/* 5) Write a SQL query to List the Customer name 
+of zone = 'Mountain' ?*/
+
+select "Customer Name" from  TELECOM_CUSTOMER
+where ZONE in ('Mountain');
+
+
+/* -------- optimization of queries ------------*/
+
 set timing on;
 /* 1)Write a SQL query to find a list of customer names
  who are using  the product Digital Subscriber Line? */
@@ -147,4 +183,56 @@ select * from table(DBMS_XPLAN.display());
 
 /*cost(%cpu) has been reduced by creating telecom_customer_name_zone_idx index */
 
+
+
+/* ----------- optimized querie -------------- * /
+
+/* 1)Write a SQL query to find a list of customer names
+ who are using  the product Digital Subscriber Line? */
+
+ -- index on product & customer name column --
+
+create index telecom_Customer_name_product_idx on TELECOM_CUSTOMER("Customer Name",product);
+
+select "Customer Name" from TELECOM_CUSTOMER
+where PRODUCT='Digital Subscriber Line';
+
+
+/* 2)Write a SQL query to  list a customerid, customer name
+ whose name starts with 'sa'? */
+
+ /* Index on customer Name column */
+
+create index telecom_Customer_name_idx on TELECOM_CUSTOMER("Customer Name");
+
+select "Customer Name" from TELECOM_CUSTOMER
+where PRODUCT='Digital Subscriber Line';
+
+
+/* 3)Write a SQL query to  list the Customer IDs and names 
+belonging to the gold customer segment? */
+
+-- index on service segment & customer id column --
+
+create index telecom_customerid_servicesegment_idx on TELECOM_CUSTOMER("Service Segment","CUSTOMERID","Customer Name");
+
+select CUSTOMERID,"Customer Name" from TELECOM_CUSTOMER
+where "Service Segment"='Gold';
+
+/* 4) Write a SQL query to Count the Customer list product-wise?  */
+
+select PRODUCT,count(PRODUCT) from TELECOM_CUSTOMER
+group by PRODUCT;
+
+
+/* 5) Write a SQL query to List the Customer name 
+of zone = 'Mountain' ?*/
+
+-- index on zone & customer Name--
+create index telecom_customer_name_zone_idx on telecom_customer("Customer Name",zone);
  
+select "Customer Name" from  TELECOM_CUSTOMER
+where ZONE in ('Mountain');
+
+
+
