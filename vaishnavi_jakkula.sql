6231e47418d8fa1eb9ac58c1262ec3f3f1d14a2b
vaishnavijakkula <jakkulavaishnavireddy@gmail.com>
2023-07-29 15:29:51 +0000

    final

--- vaishnavi_jakkula.sql
+++ vaishnavi_jakkula.sql
@@ -1,20 +1,41 @@
-/* Query-1*/
+set timing on;
+
+
+/* 1)Write a SQL query to find a list of customer names
+ who are using  the product Digital Subscriber Line? */
+
 select "Customer Name" 
 from TELECOM_CUSTOMER
 where PRODUCT='Digital Subscriber Line';
-/*Query-2*/
+
+
+/* 2)Write a SQL query to  list a customerid, customer name
+ whose name starts with 'sa'? */
+
 select CUSTOMERID,"Customer Name" 
 from TELECOM_CUSTOMER
 where "Customer Name" like'sa%';
-/*Query-3*/
+
+
+/* 3)Write a SQL query to  list the Customer IDs and names 
+belonging to the gold customer segment? */
+
 select CUSTOMERID,"Customer Name"
  from TELECOM_CUSTOMER
 where "Service Segment"='Gold';
-/*Query-4*/
+
+
+/* 4) Write a SQL query to Count the 
+Customer list product-wise?  */
+
 select PRODUCT, count(PRODUCT) 
 from TELECOM_CUSTOMER
 group by PRODUCT;
-/*Query-5*/
+
+
+/* 5) Write a SQL query to List the Customer name 
+of zone = 'Mountain' ?*/
+
 select "Customer Name" 
 from TELECOM_CUSTOMER
 where ZONE='Mountain';
