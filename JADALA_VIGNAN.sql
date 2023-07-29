ba9717c0795246145c86788ddc6941730a3a784d
JadalaVignan <20955a0421@iare.ac.in>
2023-07-29 06:22:00 +0000

    file_name_change

---
+++ JADALA_VIGNAN.sql
@@ -0,0 +1,110 @@
+/*query for customer names who are using product digital subscriber line*/
+select "Customer Name"
+from TELECOM_CUSTOMER
+where product='Digital Subscriber Line';
+/*query for customerid and customer name whose name starts with 'sa'*/
+select customerid, "Customer Name"
+FROM TELECOM_CUSTOMER
+where "Customer Name" like 'sa%';
+/*another query for customerid and customer name whose name starts with 'sa'*/
+select customerid,"Customer Name"
+from TELECOM_CUSTOMER
+where "Customer Name">='sa' and "Customer Name"<'sb';
+/*customerid and customer names beloging to the gold customer segment*/
+select customerid,"Customer Name"
+from TELECOM_CUSTOMER
+where "Service Segment"='Gold';
+/*count the customer list product wise*/
+select product, count(product) 
+from TELECOM_CUSTOMER
+group by product;
+/*another query for count the customer list product wise*/
+select product, count(*)
+from TELECOM_CUSTOMER
+group by product;
+/*query to list the customer names of the zone mountain*/
+select "Customer Name"
+from TELECOM_CUSTOMER
+where ZONE='Mountain';
+
+/*indexing of the queries*/
+DESCRIBE PLAN_TABLE;
+
+/*query for customer names who are using product digital subscriber line*/
+CREATE INDEX IDX_CNP ON TELECOM_CUSTOMER("Customer Name",PRODUCT);
+
+EXPLAIN PLAN FOR
+SELECT "Customer Name" 
+FROM TELECOM_CUSTOMER
+WHERE Product= 'Digital Subscriber Line';
+
+select*from table(DBMS_XPLAN.DISPLAY());
+
+/*query for customerid and customer name whose name starts with 'sa'*/
+CREATE INDEX IDX_CN ON TELECOM_CUSTOMER(customerid,"Customer Name");
+
+EXPLAIN PLAN FOR 
+SELECT customerid, "Customer Name"
+FROM TELECOM_CUSTOMER
+WHERE "Customer Name" like 'sa';
+
+select*from table(DBMS_XPLAN.DISPLAY());
+
+/*another query for customerid and customer name whose name starts with 'sa'*/
+EXPLAIN PLAN FOR
+SELECT customerid, "Customer Name"
+FROM TELECOM_CUSTOMER
+WHERE "Customer Name">='sa' AND "Customer Name"<'sb';
+
+select*from table(DBMS_XPLAN.DISPLAY());
+
+/*customerid and customer names beloging to the gold customer segment*/
+CREATE INDEX IDX_CS ON TELECOM_CUSTOMER(customerid,"Customer Name","Service Segment");
+
+EXPLAIN PLAN FOR
+SELECT customerid, "Customer Name"
+FROM TELECOM_CUSTOMER
+WHERE "Service Segment"='gold';
+
+select*from table(DBMS_XPLAN.DISPLAY());
+
+/*count the customer list product wise*/
+CREATE INDEX IDX_P ON TELECOM_CUSTOMER(product);
+
+EXPLAIN PLAN FOR
+SELECT PRODUCT,COUNT(PRODUCT)
+FROM TELECOM_CUSTOMER
+GROUP BY PRODUCT;
+
+select*from table(DBMS_XPLAN.DISPLAY());
+
+/*another query for count the customer list product wise*/
+EXPLAIN PLAN FOR
+SELECT PRODUCT,COUNT(*)
+FROM TELECOM_CUSTOMER
+GROUP BY PRODUCT;
+
+select*from table(DBMS_XPLAN.DISPLAY());
+
+/*query to list the customer names of the zone mountain*/
+CREATE INDEX IDX_Z ON TELECOM_CUSTOMER("Customer Name",ZONE);
+
+EXPLAIN PLAN FOR
+SELECT "Customer Name"
+FROM TELECOM_CUSTOMER
+WHERE ZONE='Mountain';
+
+select*from table(DBMS_XPLAN.DISPLAY());
+
+
+
+
+
+
+
+
+
+
+
+
+
