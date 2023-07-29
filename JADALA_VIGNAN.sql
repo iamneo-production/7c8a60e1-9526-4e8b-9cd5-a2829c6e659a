7f25d02c218b5d8c2f4c2fe0c657b5ce78b03c71
JadalaVignan <20955a0421@iare.ac.in>
2023-07-29 11:23:44 +0000

    change

--- JADALA_VIGNAN.sql
+++ JADALA_VIGNAN.sql
@@ -2,26 +2,32 @@
 select "Customer Name"
 from TELECOM_CUSTOMER
 where product='Digital Subscriber Line';
+
 --query for customerid and customer name whose name starts with 'sa'
 select customerid,"Customer Name"
 from TELECOM_CUSTOMER
 where "Customer Name">='sa' and "Customer Name"<'sb';
+
 --customerid and customer names beloging to the gold customer segment
 select customerid,"Customer Name"
 from TELECOM_CUSTOMER
 where "Service Segment"='Gold';
+
 --count the customer list product wise
 select product, count(product) 
 from TELECOM_CUSTOMER
 group by product;
+
 --another query for count the customer list product wise
 select product, count(*)
 from TELECOM_CUSTOMER
 group by product;
+
 --query to list the customer names of the zone mountain
 select "Customer Name"
 from TELECOM_CUSTOMER
 where ZONE='Mountain';
+
 
 --indexing of the queries
 DESCRIBE PLAN_TABLE;
