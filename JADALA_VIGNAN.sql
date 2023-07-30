ef27ca0a5b91574295707d76d2c2a6a915b6b50a
JadalaVignan <20955a0421@iare.ac.in>
2023-07-30 14:59:03 +0000

    final queries

--- JADALA_VIGNAN.sql
+++ JADALA_VIGNAN.sql
@@ -13,12 +13,7 @@ select customerid,"Customer Name"
 from TELECOM_CUSTOMER
 where "Service Segment"='Gold';
 
---count the customer list product wise
-select product, count(product) 
-from TELECOM_CUSTOMER
-group by product;
-
---another query for count the customer list product wise
+--query for count the customer list product wise
 select product, count(*)
 from TELECOM_CUSTOMER
 group by product;
@@ -28,13 +23,47 @@ select "Customer Name"
 from TELECOM_CUSTOMER
 where ZONE='Mountain';
 
-
---indexing of the queries
-DESCRIBE PLAN_TABLE;
+--optimization of queries using indexing
 
 --query for customer names who are using product digital subscriber line
 CREATE INDEX IDX_CNP ON TELECOM_CUSTOMER("Customer Name",PRODUCT);
 
+SELECT "Customer Name" 
+FROM TELECOM_CUSTOMER
+WHERE Product= 'Digital Subscriber Line';
+
+--query for customerid and customer name whose name starts with 'sa'
+CREATE INDEX IDX_CIN ON TELECOM_CUSTOMER(customerid,"Customer Name");
+
+SELECT customerid, "Customer Name"
+FROM TELECOM_CUSTOMER
+WHERE "Customer Name">='sa' AND "Customer Name"<'sb';
+
+--customerid and customer names beloging to the gold customer segment
+CREATE INDEX IDX_CS ON TELECOM_CUSTOMER(customerid,"Customer Name","Service Segment");
+
+SELECT customerid, "Customer Name"
+FROM TELECOM_CUSTOMER
+WHERE "Service Segment"='gold';
+
+--query count the customer list product wise
+CREATE INDEX IDX_P ON TELECOM_CUSTOMER(product);
+
+SELECT PRODUCT,COUNT(*)
+FROM TELECOM_CUSTOMER
+GROUP BY PRODUCT;
+
+--query to list the customer names of the zone mountain
+CREATE INDEX IDX_Z ON TELECOM_CUSTOMER("Customer Name",ZONE);
+
+SELECT "Customer Name"
+FROM TELECOM_CUSTOMER
+WHERE ZONE='Mountain';
+
+--plan explaination
+DESCRIBE PLAN_TABLE;
+
+--query for customer names who are using product digital subscriber line
 EXPLAIN PLAN FOR
 SELECT "Customer Name" 
 FROM TELECOM_CUSTOMER
@@ -53,8 +82,6 @@ select PLAN_TABLE_OUTPUT
 from table(DBMS_XPLAN.DISPLAY());
 
 --customerid and customer names beloging to the gold customer segment
-CREATE INDEX IDX_CS ON TELECOM_CUSTOMER(customerid,"Customer Name","Service Segment");
-
 EXPLAIN PLAN FOR
 SELECT customerid, "Customer Name"
 FROM TELECOM_CUSTOMER
@@ -63,18 +90,7 @@ WHERE "Service Segment"='gold';
 select PLAN_TABLE_OUTPUT 
 from table(DBMS_XPLAN.DISPLAY());
 
---query count the customer list product wise
-CREATE INDEX IDX_P ON TELECOM_CUSTOMER(product);
-
-EXPLAIN PLAN FOR
-SELECT PRODUCT,COUNT(PRODUCT)
-FROM TELECOM_CUSTOMER
-GROUP BY PRODUCT;
-
-select PLAN_TABLE_OUTPUT 
-from table(DBMS_XPLAN.DISPLAY());
-
---another query for count the customer list product wise
+--query for count the customer list product wise
 EXPLAIN PLAN FOR
 SELECT PRODUCT,COUNT(*)
 FROM TELECOM_CUSTOMER
@@ -84,8 +100,6 @@ select PLAN_TABLE_OUTPUT
 from table(DBMS_XPLAN.DISPLAY());
 
 --query to list the customer names of the zone mountain
-CREATE INDEX IDX_Z ON TELECOM_CUSTOMER("Customer Name",ZONE);
-
 EXPLAIN PLAN FOR
 SELECT "Customer Name"
 FROM TELECOM_CUSTOMER
